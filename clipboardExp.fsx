1909a858ef620cf461fae42d7af8844e976f8ca1
allen111 <iacopo.mattiello@gmail.com>
2015-11-23 10:16:48 +0100

    added image preview

--- clipboardExp.fsx
+++ clipboardExp.fsx
@@ -1,18 +1,37 @@
 ﻿open System.Windows.Forms
 open System.Collections
 open System.Drawing
-
+open System.Text.RegularExpressions
 let f= new Form(TopMost=true)
 f.Show()
 
 
-type aaa()=
+type aaa()as this=
     inherit UserControl()
+    do this.SetStyle(ControlStyles.AllPaintingInWmPaint 
+                     ||| ControlStyles.OptimizedDoubleBuffer, true)
+
+
+
     
+        
+    let t= new Timer(Interval=500)
+    do
+        t.Tick.Add(fun e->
+            this.Invalidate()
+            )
+        t.Start()
     
     
     override this.OnMouseMove e=
-        this.Invalidate()
+        ()
+
+
+    override this.OnKeyDown e =
+        match e.KeyCode with
+            |Keys.C->this.Invalidate()
+            |_->()
+
 
 
     override this.OnPaint e=
@@ -22,10 +41,21 @@ type aaa()=
         let a=b.GetEnumerator()
         while (a.MoveNext()) do
             g.DrawString(a.Current,this.Parent.Font,Brushes.Black,i)
-            printfn "%A" (a.Current)
-            i<-PointF(10.f,i.Y+10.f)
-
+            i<-PointF(10.f,i.Y+20.f)
+            let pattern = @"\d*\.png"
+            let regex= new Regex(pattern,RegexOptions.IgnoreCase)
+            if regex.IsMatch(a.Current) then
+                printfn "img"
+                let imgT=Image.FromFile(a.Current)
+                g.DrawImage(imgT,RectangleF(i.X,i.Y,100.f,100.f))       
+                i<-PointF(10.f,i.Y+100.f)
         
+                
+
+        (*string pattern = "d*";
+Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+if (regex.IsMatch(input))*)
 
 
 
