fb08544a5dcba33d9014dd9980d7572342c07fa7
allen111 <iacopo.mattiello@gmail.com>
2015-11-25 17:39:04 +0100

    esperimento clip update

---
+++ eventoClipUpdate.fsx
@@ -0,0 +1,26 @@
+﻿open System.Windows.Forms
+
+open System.Drawing
+
+
+//evento update clipboard
+let t=new Timer(Interval=1)
+let mutable oldMatstr=Clipboard.GetText()
+let mutable oldMatFD=Clipboard.GetFileDropList()
+let evt=new Event<System.EventArgs>()
+evt.Publish.Add(fun _->printfn "changed")
+t.Tick.Add(fun e->
+    let currMatstr=Clipboard.GetText()
+    let currMatFD=Clipboard.GetFileDropList()
+    
+    if currMatstr=oldMatstr || currMatFD=oldMatFD then
+        ()
+     else
+        evt.Trigger(new System.EventArgs())
+        
+        oldMatstr<-currMatstr
+    
+    )
+
+t.Start()
+t.Stop()
